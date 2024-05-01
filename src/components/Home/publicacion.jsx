a0f31fab1b8c96d37021ebc6ab742170516a779d
Isai Jesus <pinilloscarrascoisai@gmail.com>
2024-05-01 05:30:33 -0500

    mejora de boris

--- src/components/Home/publicacion.jsx
+++ src/components/Home/publicacion.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo4 from "../../assets/img/mas.png";
 import Publicacion_card from "./publicacion_card";
--- src/components/posts/Comentarios.jsx
+++ src/components/posts/Comentarios.jsx
@@ -1,59 +1,61 @@
+import React, { useState } from 'react';
 import logo2 from "../../assets/img/perrrfil.png";
 import './Comentarios.css';
 import logo3 from "../../assets/img/gegege.jpg";
 
 function Comentarios() {
-  return (
-    <>
-    
-      <div className="comentarios-container">
-      
-        <header>
-        
-          <div className="top-frame">
-            <h2>Publicacion de p4cman jose</h2>
-            
-            <span className="close-btn">&#10006;</span>
-          </div>
-          <hr className="divider"/>
-          <div className="header-container">
-            <div className="profile-info">
-              <div className="user-details">
-                <img src={logo2} alt="Imagen de pacman jose" />
-                <div className="user-name">
-                  <h1>p4cman jose</h1>
-                  <nav className="nav">
-                    <samp>mapap@senati.pe</samp>
-                  </nav>
-                </div>
-              </div>
-              <div className="action-buttons">
-                <button className="btn noticia">Noticia</button>
-                <button className="btn programacion">Programacion</button>
-              </div>
-              <nav className="nac">30 / 04 / 2024</nav>
-            </div>
-          </div>
-        </header>
-        <body>
-          <div className="comentario">
-            <p>Nuestra imagen personal es una parte esencial de lo que somos. Es una forma de comunicar al mundo quiénes somos y qué pensamos de nosotros mismos. De hecho, es mucho más que nuestro aspecto: nuestra imagen personal también se refiere a cómo nos comportamos y cómo nos desenvolvemos en situaciones sociales.</p>
-            <img src={logo3} alt="Imagen de comentario" className="comentario-img" />
-            
-            <div className="comentario">
-              
-              <div className="top-framee">
-                <h2>COMENTARIOS:</h2>
-              </div>
-              
-            </div>
-          </div>
-        </body>
-        
-      </div>
-    </>
-  );
-}
+    const [mostrarComentarios, setMostrarComentarios] = useState(true);
 
+    const handleGoBack = () => {
+        window.history.back(); // Retrocede como si se hubiera presionado el botón de retroceso del navegador
+    };
+
+    return (
+        <>
+            {mostrarComentarios && (
+                <div className="comentarios-container">
+                    <header>
+                        <div className="top-frame">
+                            <h2>Publicacion de p4cman jose</h2>
+                            <span className="close-btn" onClick={handleGoBack}>&#10006;</span>
+                        </div>
+                        <hr className="divider" />
+                        <br />
+                        <div className="header-container">
+                            <div className="profile-info">
+                                <div className="user-details">
+                                    <img src={logo2} alt="Imagen de pacman jose" />
+                                    <div className="user-name">
+                                        <h1>p4cman jose</h1>
+                                        <nav className="nav">
+                                            <samp>mapap@senati.pe</samp>
+                                        </nav>
+                                    </div>
+                                </div>
+                                <div className="action-buttons">
+                                    <button className="btn_noticia">Noticia</button>
+                                    <button className="btn_programacion">Programacion</button>
+                                </div>
+                                <nav className="nac">30 / 04 / 2024</nav>
+                            </div>
+                        </div>
+                        <br />
+                        <div className="texto_medio">
+                            <p>Nuestra imagen personal es una parte esencial de lo que somos. Es una forma de comunicar al mundo quiénes somos y qué pensamos de nosotros mismos. De hecho, es mucho más que nuestro aspecto: nuestra imagen personal también se refiere a cómo nos comportamos y cómo nos desenvolvemos en situaciones sociales.</p>
+                        </div>
+                        <div className="imagen_arbol">
+                            <img src={logo3} alt="Imagen de comentario" className="imagen_arbol" />
+                        </div>
+                        <div className="comentario">
+                            <div className="top-framee">
+                                <h2>COMENTARIOS:</h2>
+                            </div>
+                        </div>
+                    </header>
+                </div>
+            )}
+        </>
+    );
+}
 
 export default Comentarios;
