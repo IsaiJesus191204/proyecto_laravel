3cad1a16ba2321d6595e94afca044df4953b3fac
luis7x7 <nieblesquinaluisangel@gmail.com>
2024-05-03 01:53:00 -0500

    luis_cambios_front

--- src/components/posts/user_publicacion.jsx
+++ src/components/posts/user_publicacion.jsx
@@ -12,7 +12,8 @@ function User_publicacion() {
   const location = useLocation();
   const { publicacionData } = location.state || {};
 
-  console.log([publicacionData, "comoo"]);
+  const { data, total_comentario } = publicacionData;
+  console.log([data[0], total_comentario, "comoo"]);
 
   return (
     <div className="padre_padre_user_public">
@@ -22,40 +23,37 @@ function User_publicacion() {
           <div className="user_public_header">
             <nav className="uph_nav1">
               <p>
-                {publicacionData[0].nombre_categoria} /{" "}
-                {publicacionData[0].nombre_tema}
+                {data[0].nombre_categoria} / {data[0].nombre_tema}
               </p>
               <Link to={"/home"}>
                 <img src={logo4} className=""></img>
               </Link>
             </nav>
             <nav className="uph_nav2">
-              <h1>{publicacionData[0].titulo} </h1>
-              <h3>{publicacionData[0].sub_tema}</h3>
+              <h1>{data[0].titulo} </h1>
+              <h3>{data[0].sub_tema}</h3>
               <section>
                 <nav className="nac11">
                   <nav className="nav11">
-                    <img src={publicacionData[0].imagen_usuario}></img>
-                    <span>{publicacionData[0].email_usuario}</span>
+                    <img src={data[0].imagen_usuario}></img>
+                    <span>{data[0].email_usuario}</span>
                   </nav>
                   <nav className="nav22">
                     <Link to={"/Comentarios"}>
                       <img src={logo5}></img>
                     </Link>
 
-                    <p> 123</p>
+                    <p> {total_comentario}</p>
                   </nav>
                 </nav>
-                <nav className="nac22">
-                  {publicacionData[0].fecha_publicacion}
-                </nav>
+                <nav className="nac22">{data[0].fecha_publicacion}</nav>
               </section>
             </nav>
           </div>
         </header>
         <body className="body_user_public">
-          <img src={publicacionData[0].imagen_publicacion}></img>
-          <text>{publicacionData[0].contenido}</text>
+          <img src={data[0].imagen_publicacion}></img>
+          <text>{data[0].contenido}</text>
         </body>
       </div>
     </div>
